4dd2bcf137452335c1d598c50a0897d09ab5481a
Alaukiksaxena <saxenaalaukik93@gmail.com>
2022-11-22 14:31:16 +0100

    edidted postprocessing and segmentation, completed Test jupyter notebook

--- compositionspace/postprocessing.py
+++ compositionspace/postprocessing.py
@@ -1,33 +1,26 @@
 import pandas as pd
 import os
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import h5py    
 from sklearn.cluster import DBSCAN
 from pyevtk.hl import pointsToVTK
 from pyevtk.hl import gridToVTK
-import yaml
 
-class DataPostprocess:
+class Postprocess_data():
     
-    def __init__(self, inputfile):
-        if isinstance(inputfile, dict):
-            self.params = inputfile
-        else:
-            with open(inputfile, "r") as fin:
-                params = yaml.safe_load(fin)
-            self.params = params
-        self.version = "1.0.0"
+    def __init__(self,params):
+        self.params = params
 
-    def get_post_centroids(self, voxel_centroid_phases_files, cluster_id):
+    def get_post_centroids(self,Voxel_centroid_phases_files, cluster_id):
         
         """
         Reads the voxel centroids for a phase  
 
         Parameters
         ----------
-        voxel_centroid_phases_files : str, Voxel cetroids corresponding to each phase
+        Voxel_centroid_phases_files : str, Voxel cetroids corresponding to each phase
         cluster_id: int, phase id in Voxel_centroid_phases_files
 
         Returns
@@ -41,7 +34,7 @@ class DataPostprocess:
         """
 
 
-        with h5py.File(voxel_centroid_phases_files , "r") as hdfr:
+        with h5py.File(Voxel_centroid_phases_files , "r") as hdfr:
             group = cluster_id
             Phase_arr =  np.array(hdfr.get(f"{group}/{group}"))
             Phase_columns = list(list(hdfr.get(f"{group}").attrs.values())[0])
@@ -50,12 +43,12 @@ class DataPostprocess:
             Df_centroids = Phase_cent_df.copy()
             Df_centroids_no_files = Df_centroids.drop(['file_name'] , axis=1)
             files = Df_centroids['file_name']
+            files.values
 
-        return Df_centroids_no_files, Df_centroids, Phase_columns
+            return Df_centroids_no_files, Df_centroids, Phase_columns
 
         
-    def DBSCAN_clustering(self, voxel_centroid_phases_files, cluster_id, 
-        plot= False, plot3d = False, save =False):
+    def DBSCAN_clustering(self, cluster_id, eps, min_samples,plot= False, plot3d = False, save =False):
         """
         Get individual clusters or precipitates corresponding to each phase/ chemical domain.
         DBSCAN is applied on the centroids of the voxels helping to remove noisy voxels around clusters.
@@ -87,10 +80,10 @@ class DataPostprocess:
         -----
         input is taken from composition space based segmentation of phases.
         """        
-        eps = self.params["ml_models"]["DBScan"]["eps"]
-        min_samples = self.params["ml_models"]["DBScan"]["min_samples"]
+        OutFile_path = self.params['output_path'] 
+        Voxel_centroid_phases_files = self.params['output_path'] +"/Output_voxel_cetroids_phases.h5"
 
-        Df_centroids_no_files, Df_centroids, Phase_columns = self.get_post_centroids( voxel_centroid_phases_files ,cluster_id)
+        Df_centroids_no_files, Df_centroids, Phase_columns = self.get_post_centroids( Voxel_centroid_phases_files ,cluster_id)
 
         db = DBSCAN(eps=eps, min_samples= min_samples).fit(Df_centroids_no_files.values) #eps=5., min_samples= 35
         core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -107,34 +100,37 @@ class DataPostprocess:
 
         for i in np.unique(labels):
             if i !=-1:
+
                 cl_idx =np.argwhere(labels==i).flatten()
                 cl_cent=Df_centroids_no_files.iloc[cl_idx]
                 cl_cent["ID"] = [i]*len(cl_cent)
                 cluster_combine_lst.append(cl_cent)
 
+
         if plot3d == True: 
-            OutFile = os.path.join(self.params["output_path"], f"Output_DBSCAN_segmentation_phase{cluster_id}")
+            OutFile = OutFile_path +"/Output_DBSCAN_segmentation_phase" + f"{cluster_id}"
             Df_comb = pd.concat(cluster_combine_lst)
             image = Df_comb.values
+            FILE_PATH1 = OutFile
             x = np.ascontiguousarray(image[:,0])
             y= np.ascontiguousarray(image[:,1])
             z = np.ascontiguousarray(image[:,2])
             label = np.ascontiguousarray( image[:,3])
-            pointsToVTK(OutFile,x,y,z, data = {"label" : label}  )
+            pointsToVTK(FILE_PATH1,x,y,z, data = {"label" : label}  )
+
 
         if save == True:
-            OutFile = OutFile_path + f"Output_DBSCAN_segmentation_phase_{cluster_id}.h5"
+            OutFile = OutFile_path + f"/Output_DBSCAN_segmentation_phase_{cluster_id}.h5"
             with h5py.File(OutFile, "w") as hdfw:
+
                 G = hdfw.create_group(f"{cluster_id}")
                 G.attrs["columns"] = Phase_columns
+
                 for i in tqdm(np.unique(labels)):
                     if i !=-1:
+
                         cl_idx =np.argwhere(labels==i).flatten()
                         cl_cent=Df_centroids.iloc[cl_idx]
+                        #cluster_combine_lst.append(cl_cent)
                         G.create_dataset("{}".format(i), data = cl_cent.values)
-
-
-    
-
-
- 
+                        #l_cent.to_csv("cl_D3_Zr_{}.csv".format(i), index =False)
